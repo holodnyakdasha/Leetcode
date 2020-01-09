ececb5266dcd51d417f1fc6e24734ba03066638f
holodnyakdasha <holodnyakdasha@gmail.com>
2020-01-09 20:51:20 +0200

    solved task

--- TwoSum/index.js
+++ TwoSum/index.js
@@ -1,10 +1,10 @@
-function twoSum(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i; j < nums.length; j++) {
-            if (nums[j] == target - nums[i]) {
-                return new Array(i, j);
+const twoSum = (nums, target) => {
+    const obj = {}
+    for(let i = 0; i < nums.length; i++){
+            if(obj[nums[i]] >= 0){
+                return [obj[nums[i]], i]
             }
-        }
+        obj[target-nums[i]] = i
     }
-}
+};
 console.log(twoSum([2, 7, 11, 15], 18));
